080d19301c75301015088900f3a63a65b0e442f7
sbarrios93 <sbarrios93@gmail.com>
2023-01-22 18:00:14 -0600

    09-react-query

---
+++ src/fetchPet.js
@@ -0,0 +1,11 @@
+const fetchPet = async ({ queryKey }) => {
+  const id = queryKey[1];
+  const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
+  console.log(queryKey);
+  if (!apiRes.ok) {
+    throw new Error(`details/${id} fetch not ok`);
+  }
+  return apiRes.json();
+};
+
+export default fetchPet;
